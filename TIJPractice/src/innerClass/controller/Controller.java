6b4417d5fee7e10d4b0ef441f767f329df8d2214
Junyao Yang <ksacreym@gmail.com>
2017-07-21 16:13:28 -0400

    control framework demo

---
+++ TIJPractice/src/innerClass/controller/Controller.java
@@ -0,0 +1,22 @@
+package innerClass.controller;
+import java.util.*;
+
+public class Controller {
+	//a class from java.util to hold Event objects:
+	private List<Event> eventList = new ArrayList<Event>();
+	
+	public void addEvent(Event c) { eventList.add(c); }
+	
+	public void run() {
+		
+		while (eventList.size() > 0)
+			//Make a copy so you're not modifying the list
+			//While you're selecting the elements in it:
+			for (Event e : new ArrayList<Event>(eventList))
+				if(e.ready() ) {
+					System.out.println(e);
+					e.action();
+					eventList.remove(e);
+				}
+	}
+}
