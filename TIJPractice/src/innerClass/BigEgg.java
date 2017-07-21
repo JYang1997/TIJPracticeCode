9c8f20551216647f763b095f83b1d203aea6b43c
Junyao Yang <ksacreym@gmail.com>
2017-07-21 16:14:13 -0400

    inner class can't be overriden

---
+++ TIJPractice/src/innerClass/BigEgg.java
@@ -0,0 +1,30 @@
+package innerClass;
+//an inner class cannot be overridden like a method.
+
+class Egg {
+	private Yolk y;
+	protected class Yolk {
+		public Yolk () { System.out.println("Egg.Yolk()"); }
+	}
+	public Egg() {
+		System.out.println("New Egg()");
+		y = new Yolk();
+	}
+}
+public class BigEgg extends Egg {
+	
+
+	public class Yolk {
+		public Yolk() { System.out.println("BigEgg.Yolk()"); }
+	}
+	
+	public static void main(String [] args){
+		
+		new BigEgg();
+		//output: New Egg() \n Egg.Yolk()
+		//since BigEgg inherited from Egg when creating a big egg object, the "internal" egg 
+		//object must be created first ,then  big egg wrapped around it. 
+	}
+	
+
+}
